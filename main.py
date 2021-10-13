d0c739eb5ded6d832f2f84c6a309a8365aa33264
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-13 19:17:43 +0500

    Show fps

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 # Capturing vid (cange filename to 0 if need webcam)
 capture = cv.VideoCapture("videos/vid_test_body.3gp")
 
+pTime = 0
 
 while True:
     # Reading currunt frame
@@ -13,6 +14,14 @@ while True:
     # If can't read currunt frame, break loop
     if not success:
           break
+
+    # Calculate fps
+    Ctime = time.time()
+    fps = 1/(Ctime - pTime)
+    pTime = Ctime
+
+    # Display fps
+    cv.putText(frame, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
 
     cv.imshow("Video", frame)
     key = cv.waitKey(1)
