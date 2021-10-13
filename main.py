226f86024bfb955270a656e6fd3f1f3302351686
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-13 19:22:27 +0500

    Detect pose

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import time
 # Capturing vid (cange filename to 0 if need webcam)
 capture = cv.VideoCapture("videos/vid_test_body.3gp")
 
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+
 pTime = 0
 
 while True:
@@ -14,6 +18,15 @@ while True:
     # If can't read currunt frame, break loop
     if not success:
           break
+
+    # Convert to rgb
+    imgRgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    # Process results
+    results = pose.process(imgRgb)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
     # Calculate fps
     Ctime = time.time()
