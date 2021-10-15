ff4128d528dbdf8e804f954249eb7dcb9626ddc4
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-15 13:30:03 +0500

    Update module

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 import cv2 as cv
 import mediapipe as mp
 import time
+import pose_tracker_module as pose_tracker
 
 # Capturing vid (cange filename to 0 if need webcam)
 capture = cv.VideoCapture("videos/vid_test_body.3gp")
-
-mpPose = mp.solutions.pose
-pose = mpPose.Pose()
-mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 
 while True:
     # Reading currunt frame
-    success, frame = capture.read()
+    success, img = capture.read()
 
     # If can't read currunt frame, break loop
     if not success:
           break
 
-    # Convert to rgb
-    imgRgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    detector = pose_tracker.PoseTracker()
 
-    # Process results
-    results = pose.process(imgRgb)
-
-    if results.pose_landmarks:
-        mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+    detector.trackPose(img)
 
     # Calculate fps
     Ctime = time.time()
@@ -34,9 +26,9 @@ while True:
     pTime = Ctime
 
     # Display fps
-    cv.putText(frame, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
+    cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
 
-    cv.imshow("Video", frame)
+    cv.imshow("Video", img)
     key = cv.waitKey(1)
 
     if key==27:
