8e469f2953c901ec1d4e015bc061473893af658b
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-17 18:33:10 +0500

    Add findAngle and findPos in module

--- pose_tracker_module.py
+++ pose_tracker_module.py
@@ -23,13 +23,46 @@ class PoseTracker():
         imgRgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
         # Process results
-        results = self.pose.process(imgRgb)
+        self.results = self.pose.process(imgRgb)
 
         if draw:
-            if results.pose_landmarks:
-                self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+            if self.results.pose_landmarks:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
         else:
-            print(results.pose_landmarks)
+            print(self.results.pose_landmarks)
+
+    def findPos(self, img, draw=True):
+
+        self.lmList = []
+
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape # Get w, h
+                cx, cy = int(lm.x * w), int(lm.y * h) # Convert w, h to pixels
+                self.lmList.append([id, cx, cy]) # Add updates values to lmList
+
+                if draw:
+                    cv.circle(img, (cx, cy), 10, (0, 255, 0), cv.FILLED)
+
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        if draw:
+            cv.line(img, (x1, y1), (x2, y2), (255,255,255), 3)
+            cv.line(img, (x2, y2), (x3, y3), (255,255,255), 3)
+            cv.circle(img, (x1, y1), 10, (0,0,255), -1)
+            cv.circle(img, (x1, y1), 15, (0,0,255), 2)
+            cv.circle(img, (x2, y2), 10, (0,0,255), -1)
+            cv.circle(img, (x2, y2), 15, (0,0,255), 2)
+            cv.circle(img, (x3, y3), 10, (0,0,255), -1)
+            cv.circle(img, (x3, y3), 15, (0,0,255), 2)
+
+        
 
 # FOR TESTING PURPOSES!
 def main():
